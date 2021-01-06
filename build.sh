24f33798e260ae60eb2a63f4f6010c14377d96f7
atndko <z1281552865@gmail.com>
2021-01-06 17:02:04 +0800

    drone-scripts: Sync script for building kernel
    Signed-off-by: atndko <z1281552865@gmail.com>

---
+++ build.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+cd /drone/src/
+
+KNAME="NeptuneKernel"
+MIN_HEAD=$(git rev-parse HEAD)
+VERSION="$(cat version)-$(date +%m.%d.%y)-$(echo ${MIN_HEAD:0:8})"
+ZIPNAME="${KNAME}-$(cat version)-$(echo ${MIN_HEAD:0:8})"
+
+export LOCALVERSION="-${KNAME}-$(echo "${VERSION}")"
+export HOME=/drone
+export ARCH=arm64
+export SUBARCH=arm64
+export CLANG_PATH=$HOME/clang/bin
+export PATH="$CLANG_PATH:$PATH"
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
+export KBUILD_BUILD_USER=Vwool0xE9
+export KBUILD_BUILD_HOST=Atndko
+
+START=$(date +"%s")
+
+echo
+echo "Setting defconfig"
+echo
+
+./generator ramdisk/init.qcom.post_boot.sh init/execprog.h
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip neptune_defconfig
+
+echo
+echo "Compiling kernel"
+echo
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(nproc --all) || exit 1
+
+if [ -e arch/arm64/boot/Image.gz ] ; then
+	echo
+	echo "Building Kernel Package"
+	echo
+	rm $ZIPNAME.zip 2>/dev/null
+	rm -rf kernelzip 2>/dev/null
+	# Import Anykernel3 folder
+	mkdir kernelzip
+	echo "kernel.string=Neptune Kernel $(cat version) by Vwool0xE9
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus7
+device.name2=guacamoleb
+device.name3=OnePlus7Pro
+device.name4=guacamole
+device.name5=OnePlus7ProTMO
+device.name6=guacamolet
+device.name7=OnePlus7T
+device.name8=hotdogb
+device.name9=OnePlus7TPro
+device.name10=hotdog
+device.name11=OnePlus7TProNR
+device.name12=hotdogg
+supported.versions=10
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;" > kernelzip/props
+	cp -rp scripts/AnyKernel3/* kernelzip/
+	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
+	cd kernelzip/
+	7z a -mx9 $ZIPNAME-tmp.zip *
+	7z a -mx0 $ZIPNAME-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 $ZIPNAME-tmp.zip ../$ZIPNAME.zip
+	rm $ZIPNAME-tmp.zip
+	cd ..
+	ls -al $ZIPNAME.zip
+fi
+
+END=$(date +"%s")
+DIFF=$((END - START))
+echo -e "Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /drone/src/
+
+git clone https://github.com/kdrag0n/proton-clang -b master --depth=1 clang
